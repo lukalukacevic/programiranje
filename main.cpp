3b8dde5a640f575a962bea0d80a965af802bd52b
lukalukacevic <luka.lukacevic2010@gmail.com>
2026-09-24 17:00:27 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@ int main()
 {
 
 
-    string a[5] = {"Tesla:", "BMW:", "Mercedes:", "Audi:", "Ferrari:"};
-    string b[5] = {"Model 3, Model Y, Model S", "M4, X5, M5", "G - class", "AMG GT coupe", "CLE cabriolet", "A5", "A6", "Q7", "Portofino M, F80, 812 GTS"};
+    string a[] = {"Tesla:", "BMW:", "Mercedes:", "Audi:", "Ferrari:"};
+    string b[] = {"Model 3", "Model Y", "Model S", "M4", "X5", "M5", "G - class", "AMG GT coupe", "CLE cabriolet", "A5", "A6", "Q7", "Portofino M", "F80", "812 GTS"};
 
     for(int i=0;i<5;i++) {
+            cout << a[i] << " " << b[i] << endl;
 
-        cout << a[i] << " " << b[i] << endl;
+            for(int j=0;j<i;j++) {
+                cout << a[j] << " " << b[j] << endl;
+            }
 
     }
 
